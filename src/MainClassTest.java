9c3360dafe7af2a4d8081a27935f47c3e801a83f
mober2000 <chebik2012@gmail.com>
2021-12-25 15:24:22 +0300

    Немного отредактировал

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,20 +1,28 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass
-{
-    @Test
-    public void testGetLocalNumber()
+public class MainClassTest extends MainClass {
+    @Before
+    public void textStartTest() {
+        System.out.println("Стартуем");
+    }
+
+    @After
+    public void textEndTest()
     {
+        System.out.println("Тест завершен");
+    }
+
+    @Test
+    public void testGetLocalNumber() {
         int a = getLocalNumber();
-        if (a ==  14)
-        {
+        if (a == 14) {
             System.out.println("SuccessTest");
         } else {
-            System.out.println("ErrorTest");
+            Assert.fail("ErrorTest");
         }
-
     }
 }
-
-
 
